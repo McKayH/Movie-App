f836d93ac53ddf666cbc60ca3f42b6975e1b6864
EthanKettle <97110734+EthanKettle@users.noreply.github.com>
2022-03-30 07:41:16 -0600

    rtj

--- js/movie.js
+++ js/movie.js
@@ -20,10 +20,46 @@ function makeRecent(page) {
     console.log(RHTML)
     document.getElementById('recent').innerHTML = RHTML;
 }
-function makePopular() {
+function makePopular(page) {
     let PHTML = '';
     for (let i =0; i < 20; i++){
         PHTML += `
+        <div class="CardStyle">
+            <div class="movieImg">
+                <img src="${page.results[i].poster_path}">
+            </div>
+            <div class="info">
+                <p class="rating">${page.results[i].vote_average}</p>
+                <p class="name">${page.results[i].title}</p>
+                <p class="date">${page.results[i].release_date}</p>
+            </div>
+        </div>
+        `;
+    };
+    document.getElementById(popular).innerHTML = PHTML;
+}
+function makeTrend(page) {
+    let THTML = '';
+    for (let i =0; i < 20; i++){
+        THTML += `
+        <div class="CardStyle">
+            <div class="movieImg">
+                <img src="${page.results[i].poster_path}">
+            </div>
+            <div class="info">
+                <p class="rating">${page.results[i].vote_average}</p>
+                <p class="name">${page.results[i].title}</p>
+                <p class="date">${page.results[i].release_date}</p>
+            </div>
+        </div>
+        `;
+    };
+    document.getElementById(trending).innerHTML = THTML;
+}
+function makeSave(save) {
+    let SHTML = `<div class="ScrollBar">`;
+    for (let i = 0; i < 20 && i < saved.length; i++){
+        SHTML += `
         <div class="CardStyle">
             <div class="movieImg">
                 <img src="${pages[1].results.i.poster_path}">
@@ -36,55 +72,26 @@ function makePopular() {
         </div>
         `;
     };
-    document.getElementById(popular).innerHTML = PHTML;
+    SHTML += `</div>`
+    document.getElementById(saved).innerHTML = SHTML;
 }
-function makeTrend() {
-    let THTML = '';
-    for (let i =0; i < 20; i++){
-        THTML += `
-        <div class="CardStyle">
-            <div class="movieImg">
-                <img src="${pages[1].results.i.poster_path}">
-            </div>
-            <div class="info"
-                <p class="rating">${pages[1].results.i.vote_average}</p>
-                <p class="name">${pages[1].results.i.title}</p>
-                <p class="date">${pages[1].results.i.release_date}</p>
-            </div>
-        </div>
-        `;
-    };
-    document.getElementById(trending).innerHTML = THTML;
-}
-// function makeSave(save) {
-//     let SHTML = `<div class="ScrollBar">`;
-//     for (let i = 0; i < 20 && i < saved.length; i++){
-//         SHTML += `
-//         <div class="CardStyle">
-//             <div class="movieImg">
-//                 <img src="${pages[1].results.i.poster_path}">
-//             </div>
-//             <div class="info">
-//                 <p class="rating">${pages[1].results.i.vote_average}</p>
-//                 <p class="name">${pages[1].results.i.title}</p>
-//                 <p class="date">${pages[1].results.i.release_date}</p>
-//             </div>
-//         </div>
-//         `;
-//     };
-//     SHTML += `</div>`
-//     document.getElementById(saved).innerHTML = SHTML;
-// }
 // MOVIE DETAILS
 
-// function findPop() {
-//     let popular = [];
-//     for (let i = 0; i < total_pages.length; i++) {
-//         for (let j = 0; j < results.length; j++) {
-//             popular.push()
-//             while (con != popular.length-1){
-//                 for (let k = 0; k < popular.length-1; k++)
-//             }
-//         }
-//     };
-// };
+function findPop () {
+    let pop = [];
+    while (pop.length != 20) {
+        if (vote_average >= 7) {
+            pop.push()
+        }
+    }
+    return pop
+}
+
+function findTrend () {
+    let trend = [];
+    while (trend.length != 20) {
+        if (release_date >= 2020) {
+
+        }
+    }
+}
