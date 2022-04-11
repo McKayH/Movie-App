750cd4443b5cd9d30d0d2f09740ed0cebc34a430
EthanKettle <97110734+EthanKettle@users.noreply.github.com>
2022-04-11 08:04:12 -0600

    m

--- js/fetch.js
+++ js/fetch.js
@@ -73,37 +73,3 @@ function minus(pg, fun) {
         fun(data)
     });
 }
-
-//  work on this later
-
-
-// function sortIds(ap){
-//     const page = ap;
-//     let ids = [];
-//     page.results.forEach(ele => {
-//         ids.push(ele.id);
-//         });
-//     sortIds(ids);
-//     function sortIds(arr){
-//         let i = 0;
-//         while(arr.length){
-//             const min = smallest(arr)
-//             const smallId = arr.splice(arr.indexOf(min), 1);
-//             sortedIds.push(smallId[0]);
-//             i++;
-//             if(i === 100){
-//                 break;
-//             }
-//         }
-//         function smallest(arr){
-//             let small = arr[0];
-//             arr.forEach(ele =>{
-//                 if(ele < small){
-//                     small = ele;
-//                 }
-//             });
-//             return small;
-//         }
-//         console.log(sortedIds);
-//     }
-// }
