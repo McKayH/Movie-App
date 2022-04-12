a4410098fa775baae7d2b32643bfaa172facc382
McKay H <admin@McKay.local>
2022-04-12 08:44:35 -0600

    nothing

--- js/savedMv.js
+++ js/savedMv.js
@@ -6,10 +6,7 @@ function removeSv(id){
     let ids = [];
     saved.map(obj =>{ids.push(obj.id);});
     const indx = ids.indexOf(id.toString());
-    console.log(indx); 
-    console.log(saved[indx]);
     saved.splice(indx, 1);
-    console.log(saved);
     localStorage.setItem('movieList', JSON.stringify(saved));
 }
 function displayMv(){
