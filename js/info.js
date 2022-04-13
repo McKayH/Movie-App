fa7564b0077c9bcb3ee60c173b89f4f36b148a36
McKay H <admin@McKay.local>
2022-04-13 09:56:32 -0600

    added detail

--- js/info.js
+++ js/info.js
@@ -4,24 +4,40 @@ function switchPage(id){
     console.log(id);
     const movie = fetchById(id);
     movie.then(data =>{
-
-        makePage(data.poster_path, data.title, data.release_date, data.vote_average, data.overview);
+        makePage(data, vote);
     });
 
 
 }
-function makePage(img, title, date, rating, info) {
-    console.log('here');
-    HTML = `
+function makePage(data) {
+    let poster = '';
+    let genres = ''
+    console.log(data)
+    if (!data.poster_path) {
+        poster = '<img src="img/noPost.png">';
+    }
+    else{
+        poster = `<img src="https://image.tmdb.org/t/p/w500${data.poster_path}">`;
+    }
+    data.genres.forEach(ele =>{
+        genres += `<li>${ele.name}</li>`
+    });
+
+
+
+
+
+    const HTML = `
     <button onClick="window.location.reload();">Go Back</button>
     <div class="centerStuff">
-        
-            <img src="https://image.tmdb.org/t/p/w500${img}">
+        <h1>${data.title}</h1>
+            ${poster}
             <div class="infomation">
-                <div>${title}</div>
-                <div>${info}</div>
-                <div>${date}</div>
-                <div>${rating}</div>
+                <h5>Genres</h5>
+                <ul id="genre">${genres}</ul>
+                <p>${data.overview}</p>
+                <div>${data.release_date}</div>
+                <div>Average rating: ${data.vote_average}<div> Votes counted: ${data.vote_count}</div></div>
             </div>
         
     </div>`;
